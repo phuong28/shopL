b69515ba7c152f72ff314e46345cdc099efebc8a
your name <ninhphuong2k1nb@gmail.com>
2022-08-31 00:19:58 +0700

    finished hompage

--- app/Repositories/ProductsRepository.php
+++ app/Repositories/ProductsRepository.php
@@ -16,17 +16,17 @@ class ProductsRepository extends BaseRepository
     }
     public function showProducts()
     {
-        $products = $this->model->paginate(2);
+        $products = $this->model->paginate(4);
         return $products;
     }
     public function topSell()
     {
         //SELECT`order_detail`. product_name as name, Sum(`order_detail`.quantity) as quantities , products.images as image FROM `order_detail` INNER JOIN products on products.products_id=order_detail.product_id GROUP by product_id ORDER by quantities DESC Limit 4;
         $topSell = $this->builder()
-            ->select('od.product_name as name', DB::raw('Sum(od.quantity) as quantities'), 'pd.images as image')
+            ->select('od.product_id as products_id','od.product_name as name', DB::raw('Sum(od.quantity) as quantities'), 'pd.images as image')
             ->from('products as pd')
             ->join('order_detail as od', 'od.product_id', '=', 'pd.products_id')
-            ->groupBy('od.product_name', 'pd.images')
+            ->groupBy('od.product_name', 'pd.images','od.product_id')
             ->orderBy('quantities', 'DESC')
             ->limit(6)
             ->get();
